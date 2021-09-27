426f815a116955a930a3ce27be18997ca65db958
Alimur Razi Rana <alimurrazi570@gmail.com>
2021-09-27 21:30:41 +0600

    feat: fe get api response from be

--- DemoApp/src/app/app.component.ts
+++ DemoApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DemoApp';
+  response = 'loading';
+  constructor(private http: HttpClient){
+    this.http.get('http://localhost:5000/demo',{responseType: 'text'}).subscribe((response:any)=>{
+      console.log(response);
+      this.response = response;
+    })
+  }
 }
